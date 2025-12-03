0babf340131b6280e31bb43e148af57bb068e486
Erothu Uday kumar <158418561+Udaykumar-06@users.noreply.github.com>
2025-12-03 16:34:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Smooth scroll for Hire Me button
+document.getElementById('hireBtn').addEventListener('click', function() {
+  document.getElementById('contact').scrollIntoView({behavior:'smooth'});
+});
